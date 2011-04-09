58c8929a21ae6564bcb6ff190fed1882691a9a9b
Metin Yorulmaz <myorulmaz@gmail.com>
2011-04-09 00:12:00 -0400

    MySql conection

--- HR/Controllers/HomeController.cs
+++ HR/Controllers/HomeController.cs
@@ -11,9 +11,18 @@ namespace HR.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
+            MySql.Data.MySqlClient.MySqlConnection c = new MySql.Data.MySqlClient.MySqlConnection();
+            c.ConnectionString = "Server=localhost;Database=test;Uid=ODBC;Pwd=;";
+            MySql.Data.MySqlClient.MySqlCommand cmd = c.CreateCommand();
+            cmd.CommandText = "Select * from tbl1";
+            c.Open();
+            cmd.ExecuteReader();
+            c.Close();
+
 
             return View();
         }
+
 
         public ActionResult About()
         {
